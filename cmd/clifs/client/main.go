a1afbbdcf1544f36d321ebb42fdc5fa06e321ad6
Niels Jaspers <gh@nielsjaspers.com>
2025-11-20 18:50:50 +0100

    feat: add main files for client and server

---
+++ cmd/clifs/client/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nielsjaspers/clifs/internal/client"
+
+func main() {
+	client.HandleClient("localhost:443")
+}
---
+++ cmd/clifs/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nielsjaspers/clifs/internal/server"
+)
+
+func main() {
+	fmt.Printf("Hello World\n")
+	server.HandleServer()
+}
+
