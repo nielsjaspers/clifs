937f408bea896995d67929d8630395a1ee7d77d9
Niels Jaspers <gh@nielsjaspers.com>
2025-11-20 19:14:14 +0100

    feat: add uploadHandler to server

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,13 +3,18 @@ package server
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/nielsjaspers/clifs/internal/keygen"
 )
 
 func HandleServer() {
+	// TODO: Change key generation to flag instead on every start
 	err := keygen.GenerateKeys()
 	if err != nil {
 		log.Fatalf("Error creating keys: %v\n", err)
@@ -19,6 +24,7 @@ func HandleServer() {
 	keyFile := "server-env/key.pem"
 
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/upload", handleUpload)
 
 	server := &http.Server{
 		Addr:      ":443",
@@ -33,5 +39,49 @@ func HandleServer() {
 }
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", req.URL.Path[1:])
+	fmt.Fprintf(w, "Hello, %s!", req.URL.Path[1:])
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	// Only allow POST requests
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse multipart form (up to 32MB)
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
+
+	// Get file from request
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Failed to get file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	fileName := timestamp + header.Filename
+	safeFilename := filepath.Clean(fileName)
+
+	// Create the destination file
+	// TODO: change to use config.UploadDir
+	dst, err := os.Create(filepath.Join("./uploads", safeFilename))
+	if err != nil {
+		http.Error(w, "Failed to create file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	// Copy the file data
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, "Failed to save file: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Received file: %s (size: %d bytes)", header.Filename, header.Size)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "File uploaded successfully: %s", safeFilename)
 }
